350d9c296d0f87ad9144de13fe7288d6b00510f3
Ana Flechas <131080747+anita0705@users.noreply.github.com>
2026-03-12 16:25:29 -0500

    Update LoginResponse.java

--- ms-auth/src/main/java/com/tiendagenerica/ms_auth/dto/LoginResponse.java
+++ ms-auth/src/main/java/com/tiendagenerica/ms_auth/dto/LoginResponse.java
@@ -6,8 +6,43 @@ import lombok.AllArgsConstructor;
 @Data
 @AllArgsConstructor
 public class LoginResponse {
-    private String token;
-    private String username;
-    private String rol;
-    private String mensaje;
+	private String token;
+	private String username;
+	private String mensaje;
+
+	public LoginResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginResponse(String token, String username, String mensaje) {
+		super();
+		this.token = token;
+		this.username = username;
+		this.mensaje = mensaje;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
 }
