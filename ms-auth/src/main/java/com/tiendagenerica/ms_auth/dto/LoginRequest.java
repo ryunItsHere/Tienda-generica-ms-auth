a90190048d19a0ce459587829a8bd857880979bc
Ana Flechas <131080747+anita0705@users.noreply.github.com>
2026-03-12 16:24:57 -0500

    Update LoginRequest.java

--- ms-auth/src/main/java/com/tiendagenerica/ms_auth/dto/LoginRequest.java
+++ ms-auth/src/main/java/com/tiendagenerica/ms_auth/dto/LoginRequest.java
@@ -4,6 +4,33 @@ import lombok.Data;
 
 @Data
 public class LoginRequest {
-    private String username;
-    private String password;
+	private String username;
+	private String password;
+
+	public LoginRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginRequest(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
