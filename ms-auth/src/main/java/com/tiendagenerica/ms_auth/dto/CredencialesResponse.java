ba6d08de6b0cab19440aecbbc20432cb9e54d5ec
Ana Flechas <131080747+anita0705@users.noreply.github.com>
2026-03-12 16:23:48 -0500

    Update CredencialesResponse.java

--- ms-auth/src/main/java/com/tiendagenerica/ms_auth/dto/CredencialesResponse.java
+++ ms-auth/src/main/java/com/tiendagenerica/ms_auth/dto/CredencialesResponse.java
@@ -4,8 +4,48 @@ import lombok.Data;
 
 @Data
 public class CredencialesResponse {
-    private String username;
-    private String password;  // encriptada
-    private String rol;       // ADMIN o EMPLEADO
-    private boolean activo;
+
+	private String username;
+	private String password;
+	private String rol;
+	private boolean activo;
+	
+	public CredencialesResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
+	
+
 }
